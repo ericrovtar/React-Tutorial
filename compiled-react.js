b2711b80b31c9251f2b8690a82d214b9e4abd902
Eric Rovtar <eric.rovtar@me.com>
2016-12-19 15:43:30 -0500

    FInish tutorial

--- compiled-react.js
+++ compiled-react.js
@@ -1,62 +1,154 @@
 'use strict';
 
-function FancyBorder(props) {
+function ProductRow(props) {
     return React.createElement(
-        'div',
-        { className: 'FancyBorder FancyBorder-' + props.color },
-        props.children
+        "tr",
+        null,
+        React.createElement(
+            "td",
+            null,
+            props.product.name
+        ),
+        React.createElement(
+            "td",
+            null,
+            props.product.price
+        )
     );
 }
 
-function Dialog(props) {
-    return React.createElement(
-        FancyBorder,
-        { color: 'blue' },
-        React.createElement(
-            'h1',
-            { className: 'Dialog-title' },
-            props.title
-        ),
-        React.createElement(
-            'p',
-            { className: 'Dialg-message' },
-            props.message
-        ),
-        props.children
-    );
+class ProductCategoryRow extends React.Component {
+    render() {
+        return React.createElement(
+            "tr",
+            null,
+            React.createElement(
+                "th",
+                { colSpan: "2" },
+                this.props.title
+            )
+        );
+    }
 }
 
-class SignUpDialog extends React.Component {
+class ProductTable extends React.Component {
+    render() {
+        var rows = [];
+        var lastCategory = "";
+
+        this.props.products.forEach(product => {
+            //Check for filters
+            if (product.name.indexOf(this.props.filterText) === -1 || !product.stocked && this.props.inStockOnly) {
+                //Not going to display you
+                return;
+            }
+
+            //See if we have a new category
+            if (lastCategory !== product.category) {
+                //Create new ProductCategoryRow
+                rows.push(React.createElement(ProductCategoryRow, { title: product.category }));
+
+                //Update lastCategory
+                lastCategory = product.category;
+            }
+
+            rows.push(React.createElement(ProductRow, { product: product }));
+        });
+
+        return React.createElement(
+            "table",
+            null,
+            React.createElement(
+                "thead",
+                null,
+                React.createElement(
+                    "th",
+                    null,
+                    "Name"
+                ),
+                React.createElement(
+                    "th",
+                    null,
+                    "Price"
+                )
+            ),
+            React.createElement(
+                "tbody",
+                null,
+                rows
+            )
+        );
+    }
+}
+
+class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSignUp = this.handleSignUp.bind(this);
-        this.state = { login: '' };
+    }
+
+    handleChange() {
+        this.props.onUserInput(this.filterTextInput.value, this.inStockOnlyInput.checked);
     }
 
     render() {
         return React.createElement(
-            Dialog,
-            {
-                title: 'Welcome',
-                message: 'Thank you for visiting our spacecraft!' },
-            React.createElement('input', { value: this.state.login,
+            "form",
+            null,
+            React.createElement("input", {
+                type: "text",
+                placeholder: "Search...",
+                value: this.props.filterText,
+                ref: input => this.filterTextInput = input,
                 onChange: this.handleChange }),
+            React.createElement("br", null),
             React.createElement(
-                'button',
-                { onClick: this.handleSignUp },
-                'Sign Me Up!'
+                "label",
+                null,
+                React.createElement("input", {
+                    type: "checkbox",
+                    checked: this.props.inStockOnly,
+                    ref: input => this.inStockOnlyInput = input,
+                    onChange: this.handleChange }),
+                "Only show products in stock"
             )
         );
     }
+}
 
-    handleChange(e) {
-        this.setState({ login: e.target.value });
+class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
     }
 
-    handleSignUp() {
-        alert(`Welcome aboard, ${ this.state.login }!`);
+    handleUserInput(filterText, inStockOnly) {
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    }
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(SearchBar, {
+                filterText: this.state.filterText,
+                inStockOnly: this.state.inStockOnly,
+                onUserInput: this.handleUserInput }),
+            React.createElement(ProductTable, {
+                products: this.props.products,
+                filterText: this.state.filterText,
+                inStockOnly: this.state.inStockOnly })
+        );
     }
 }
 
-ReactDOM.render(React.createElement(SignUpDialog, null), document.getElementById('root'));
+var PRODUCTS = [{ category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' }, { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' }, { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' }, { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' }, { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' }, { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }];
+
+ReactDOM.render(React.createElement(FilterableProductTable, { products: PRODUCTS }), document.getElementById('root'));
--- react.js
+++ react.js
@@ -1,60 +1,142 @@
 'use strict';
 
-function FancyBorder(props) {
+function ProductRow(props) {
     return (
-        <div className={'FancyBorder FancyBorder-' + props.color}>
-            {props.children}
-        </div>
+        <tr>
+            <td>{props.product.name}</td>
+            <td>{props.product.price}</td>
+        </tr>
     );
 }
 
-function Dialog(props) {
-    return (
-        <FancyBorder color="blue">
-            <h1 className="Dialog-title">
-                {props.title}
-            </h1>
-            <p className="Dialg-message">
-                {props.message}
-            </p>
-            {props.children}
-        </FancyBorder>
-    )
+class ProductCategoryRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <th colSpan="2">{this.props.title}</th>
+            </tr>
+        );
+    }
 }
 
-class SignUpDialog extends React.Component {
+class ProductTable extends React.Component {
+    render() {
+        var rows = [];
+        var lastCategory = "";
+
+        this.props.products.forEach((product) => {
+            //Check for filters
+            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+                //Not going to display you
+                return; 
+            }
+
+            //See if we have a new category
+            if (lastCategory !== product.category) {
+                //Create new ProductCategoryRow
+                rows.push(<ProductCategoryRow title={product.category} />);
+                
+                //Update lastCategory
+                lastCategory = product.category;
+            }
+
+            rows.push(<ProductRow product={product} />);
+        });
+
+        return (
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Price</th>
+                </thead>
+                
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSignUp = this.handleSignUp.bind(this);
-        this.state = {login: ''};
+    }
+
+    handleChange() {
+        this.props.onUserInput(
+            this.filterTextInput.value,
+            this.inStockOnlyInput.checked
+        )
     }
 
     render() {
         return (
-            <Dialog
-                title="Welcome"
-                message="Thank you for visiting our spacecraft!">
-                <input value={this.state.login}
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="Search..." 
+                    value={this.props.filterText} 
+                    ref={(input) => this.filterTextInput = input}
                     onChange={this.handleChange} />
-                <button onClick={this.handleSignUp}>
-                    Sign Me Up!
-                </button>
-            </Dialog>
+                <br />
+                <label>
+                    <input 
+                        type="checkbox" 
+                        checked={this.props.inStockOnly}
+                        ref={(input) => this.inStockOnlyInput = input}
+                        onChange={this.handleChange} />
+                    Only show products in stock
+                </label>
+            </form>
         );
-    }
-    
-    handleChange(e) {
-        this.setState({login: e.target.value});
-    }
-
-    handleSignUp() {
-        alert(`Welcome aboard, ${this.state.login}!`);
     }
 }
 
+class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            filterText : '', 
+            inStockOnly : false 
+        };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+
+    handleUserInput(filterText, inStockOnly) {
+        this.setState({
+            filterText : filterText,
+            inStockOnly : inStockOnly
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar 
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onUserInput={this.handleUserInput} />
+                <ProductTable 
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly} />
+            </div>
+        );
+    }
+}
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 ReactDOM.render(
-    <SignUpDialog />,
+    <FilterableProductTable products={PRODUCTS} />,
     document.getElementById('root')
 );
